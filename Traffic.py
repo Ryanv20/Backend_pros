1b7123873e1ddf1ba8dd083a727580a8308d539d
Ryanv20 <kryan2743@gmail.com>
2024-10-10 02:58:25 +0100

    Initial commit

---
+++ Traffic.py
@@ -0,0 +1,22 @@
+def main():
+    class Road:
+        #i know that lenght is length
+        def _init_(this, lenght):
+            this.lenght = lenght
+           
+            this.cars = []
+            this.r_block = [] * lenght
+
+        def add_car(self, car, position)                                                                                                                                                                               v )
+            if self.r_block[position] is None: #If the position is empty
+                self.r_block[position] = car # assigns car to a block
+                
+
+
+
+
+
+
+
+
+main()
